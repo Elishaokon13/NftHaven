4be3ad72527422966b590cf2ef399f848be85463
eliscodavid@gmail.com <eliscodavid@gmail.com>
2022-06-05 10:26:16 +0100

    logic

--- static/logic.js
+++ static/logic.js
@@ -1,5 +1,5 @@
-Moralis.initialize(""); // Application id from moralis.io
-Moralis.serverURL = ""; //Server url from moralis.io
+Moralis.initialize("uz9z3fvxaseMQ30itTCGhUfho7aMgTnpVTa5dvFj"); // Application id from moralis.io
+Moralis.serverURL = "https://g9cjusa8vigx.usemoralis.com:2053/server"; //Server url from moralis.io
 
 const nft_market_place_address = "" //NFT Market Place Contract, code of this contract is in the following github repository https://github.com/DanielMoralisSamples/25_NFT_MARKET_PLACE. 
 
